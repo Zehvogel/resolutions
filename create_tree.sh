79f499d5bab7b0e4a74fb6083e23a04b3d60d6ec
Leonhard Reichenbach <leonhard.reichenbach@cern.ch>
2023-04-20 15:21:10 +0200

    rename sim check step

--- create_tree.sh
+++ create_tree.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-fccanalysis run test.py --output test.root --files-list sim_output/single_e-_10GeV_90deg_edm4hep.root
+fccanalysis run check_sim.py --output sim_check_output/test.root --files-list sim_output/single_e-_10GeV_90deg_edm4hep.root
