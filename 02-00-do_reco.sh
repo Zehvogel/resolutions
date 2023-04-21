eb1ce5fd8674b5bf9075c9e603418121cbb9ff68
Leonhard Reichenbach <leonhard.reichenbach@cern.ch>
2023-04-21 08:58:31 +0200

    set number of event to be reconstructed

--- 02-00-do_reco.sh
+++ 02-00-do_reco.sh
@@ -3,4 +3,4 @@ set -e
 
 mkdir reco_output || true
 
-k4run fccRec_e4h_input.py --input.EventDataSvc sim_output/single_e-_10GeV_90deg_edm4hep.root --filename.PodioOutput reco_output/single_e-_10GeV_90deg_rec_edm4hep.root
+k4run fccRec_e4h_input.py -n 1000 --input.EventDataSvc sim_output/single_e-_10GeV_90deg_edm4hep.root --filename.PodioOutput reco_output/single_e-_10GeV_90deg_rec_edm4hep.root
