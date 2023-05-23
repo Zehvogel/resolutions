3d554c91c6d6e7065665a7d3c2834da10f0c7d0e
Leonhard Reichenbach <leonhard.reichenbach@cern.ch>
2023-05-23 17:13:56 +0200

    update

---
+++ setup-nightlies-fixed.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+
+. setup-nightlies.sh
+
+export LD_LIBRARY_PATH=/cvmfs/sw-nightlies.hsf.org/key4hep/releases/2023-05-20/x86_64-almalinux9-gcc11.3.1-opt/k4marlinwrapper/5006557c64208c40429179d8ab46055e756641be=develop-qol5vj/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/cvmfs/sw-nightlies.hsf.org/key4hep/releases/2023-05-20/x86_64-almalinux9-gcc11.3.1-opt/gaudi/36.10-epiyua/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/cvmfs/sw-nightlies.hsf.org/key4hep/releases/2023-05-20/x86_64-almalinux9-gcc11.3.1-opt/dd4hep/1.23-jxgcgq/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/cvmfs/sw-nightlies.hsf.org/key4hep/releases/2023-05-20/x86_64-almalinux9-gcc11.3.1-opt/fccanalyses/c67c6cd138398879b3153074bea0be1250bcb13d=develop-6r2rys/lib:$LD_LIBRARY_PATH
